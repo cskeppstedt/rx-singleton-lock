8ecadd0d9485e74fbc6ab48f4d7b324eadf57a0a
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2018-05-01 21:24:14 +0200

    Add new build, do not build the test folder

--- dist/test/utils/elements-html.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (elems: NodeListOf<Element>) => any[];
-export default _default;
--- dist/test/utils/elements-html.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (elems) {
-    var texts = [];
-    for (var i = 0; i < elems.length; i++) {
-        texts.push(elems[i].innerHTML);
-    }
-    return texts;
-});
