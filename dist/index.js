86f2e54afbc878ea1c93d9d6aec809d2734378fc
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2018-05-02 21:50:47 +0200

    Add module.exports for commonjs

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var rx_singleton_lock_1 = require("./lib/rx-singleton-lock");
+module.exports = rx_singleton_lock_1.default;
 exports.default = rx_singleton_lock_1.default;
