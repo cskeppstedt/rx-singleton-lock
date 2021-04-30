bac317a26a7e731e63303e6ad5aa62cc091d407f
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2021-04-30 13:20:46 +0200

    Build new version

--- dist/lib/utils/tracing.d.ts
+++ dist/lib/utils/tracing.d.ts
@@ -1,7 +1,7 @@
-import { Scheduler } from "rxjs";
+import { SchedulerLike } from "rxjs";
 export declare type TraceErr = (message: string, error: Error) => any;
 export declare type TraceLog = (message: string) => any;
 export declare type methodType = "singleton" | "sync";
 export declare const noop: () => void;
-export declare const createLog: (traceLog?: TraceLog, scheduler?: Scheduler) => (seq: number, method: methodType, msg: string) => any;
-export declare const createErr: (traceErr?: TraceErr, scheduler?: Scheduler) => (seq: number, method: methodType, msg: string, e: Error) => any;
+export declare const createLog: (traceLog?: TraceLog, scheduler?: SchedulerLike) => (seq: number, method: methodType, msg: string) => any;
+export declare const createErr: (traceErr?: TraceErr, scheduler?: SchedulerLike) => (seq: number, method: methodType, msg: string, e: Error) => any;
